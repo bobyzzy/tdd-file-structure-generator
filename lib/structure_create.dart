f3671f379a490c0671e92fed8d81d62d9fdb0712
Bobur Ziyodullaev <62748023+bobyzzy@users.noreply.github.com>
2024-09-18 18:59:55 +0500

    changes: minor changes

--- lib/structure_create.dart
+++ lib/structure_create.dart
@@ -25,114 +25,114 @@ void addCustomStructure(String projectName) async {
 
   createFile(
     'lib/core/bloc/bloc_scope.dart',
-    content: await readContentFromFile('assets/bloc_scope.txt'),
+    content: await readContentFromFile('lib/assets/bloc_scope.txt'),
   );
 
   createFile(
     'lib/core/bloc/bloc_observer.dart',
-    content: await readContentFromFile('assets/bloc_observer.txt'),
+    content: await readContentFromFile('lib/assets/bloc_observer.txt'),
   );
 
   /* ============================= CONSTANTS FILE CREATE ========================================*/
 
   createFile(
     'lib/core/constants/constatns.dart',
-    content: await readContentFromFile('assets/constants.txt'),
+    content: await readContentFromFile('lib/assets/constants.txt'),
   );
 
   /* =========================== CONFIG FILES CREATE ========================================*/
 
   createFile(
     'lib/core/config/local_config.dart',
-    content: await readContentFromFile('assets/local_config.txt'),
+    content: await readContentFromFile('lib/assets/local_config.txt'),
   );
 
   createFile(
     'lib/core/config/network_config.dart',
-    content: await readContentFromFile('assets/network_config.txt'),
+    content: await readContentFromFile('lib/assets/network_config.txt'),
   );
 
   /* =========================== ROUTES FILES CREATE ========================================*/
 
   createFile(
     'lib/core/routes/app_router.dart',
-    content: await readContentFromFile('assets/app_router.txt'),
+    content: await readContentFromFile('lib/assets/app_router.txt'),
   );
 
   createFile(
     'lib/core/routes/navigation_observer.dart',
-    content: await readContentFromFile('assets/navigation_observer.txt'),
+    content: await readContentFromFile('lib/assets/navigation_observer.txt'),
   );
 
   /* ============================= DI FILE CREATE ========================================*/
 
   createFile(
     'lib/core/di/service_locator.dart',
-    content: await readContentFromFile('assets/di.txt'),
+    content: await readContentFromFile('lib/assets/di.txt'),
   );
 
   /* ============================= ERRORS FILE CREATE ========================================*/
 
   createFile(
     'lib/core/errors/exeptions.dart',
-    content: await readContentFromFile('assets/exeptions.txt'),
+    content: await readContentFromFile('lib/assets/exeptions.txt'),
   );
 
   createFile(
     'lib/core/errors/failure.dart',
-    content: await readContentFromFile('assets/failure.txt'),
+    content: await readContentFromFile('lib/assets/failure.txt'),
   );
 
   /* =========================== EXTENSION FILE CREATE ========================================*/
 
   createFile(
     'lib/core/extensions/extension.dart',
-    content: await readContentFromFile('assets/extension.txt'),
+    content: await readContentFromFile('lib/assets/extension.txt'),
   );
 
   /* ============================= ENUMS FILE CREATE ========================================*/
 
   createFile(
     'lib/core/enums/enums.dart',
-    content: await readContentFromFile('assets/enums.txt'),
+    content: await readContentFromFile('lib/assets/enums.txt'),
   );
 
 /* =========================== USECASE FILE CREATE ========================================*/
 
   createFile(
     'lib/core/usecases/usecase.dart',
-    content: await readContentFromFile('assets/usecase.txt'),
+    content: await readContentFromFile('lib/assets/usecase.txt'),
   );
 
 /* ============================= UTILS FILE CREATE ========================================*/
 
   createFile(
     'lib/core/utils/validators.dart',
-    content: await readContentFromFile('assets/validator.txt'),
+    content: await readContentFromFile('lib/assets/validator.txt'),
   );
   createFile(
     'lib/core/utils/formatters.dart',
-    content: await readContentFromFile('assets/formatters.txt'),
+    content: await readContentFromFile('lib/assets/formatters.txt'),
   );
 
 /* ============================= THEMES FILE CREATE ========================================*/
 
   createFile(
     'lib/core/themes/app_diemens.dart',
-    content: await readContentFromFile('assets/app_diemens.txt'),
+    content: await readContentFromFile('lib/assets/app_diemens.txt'),
   );
 
   createFile(
     'lib/core/themes/app_colors.dart',
-    content: await readContentFromFile('assets/app_colors.txt'),
+    content: await readContentFromFile('lib/assets/app_colors.txt'),
   );
   createFile(
     'lib/core/themes/app_fonts.dart',
-    content: await readContentFromFile('assets/app_fonts.txt'),
+    content: await readContentFromFile('lib/assets/app_fonts.txt'),
   );
 
   createFile(
     'lib/core/themes/app_theme.dart',
-    content: await readContentFromFile('assets/app_theme.txt'),
+    content: await readContentFromFile('lib/assets/app_theme.txt'),
   );
 }
